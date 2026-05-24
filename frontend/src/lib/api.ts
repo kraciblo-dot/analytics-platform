fc78f7cfed0d1d6316d49cc7138f06993beec1d1
Chakri(Kraci) <kraciblo@gmail.com>
2026-05-24 10:36:54 +0530

    fix route

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 // Create a configured axios instance
+
 export const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api',
-  // CRITICAL: This tells the browser to always send the HttpOnly cookie
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   withCredentials: true, 
 });
 
